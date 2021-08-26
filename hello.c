748fd4cab4e302817417e40aa7100bd84a0c2969
ATHester <ahester2@huskers.unl.edu>
2021-08-26 19:33:44 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Aden Hester
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+    printf("Hello world!\n");
+
+    return 0;
+ }
